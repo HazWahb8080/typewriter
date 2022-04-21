e306a9016e3d94df420f813de3ccb7f336f91d19
HazWahb8080 <hazem.wahb100@gmail.com>
2022-04-21 02:30:06 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import gsap from 'gsap';
+import { useEffect } from 'react';
+import TextPlugin from 'gsap/TextPlugin';
+gsap.registerPlugin(TextPlugin);
+
+const words = ["Hazem.","Passionate.","Developer."]
 
 function App() {
+  useEffect(()=>{
+    let cursor = gsap.to(".cursor",{
+      opacity:0,
+      ease:"power2",
+      repeat : -1,
+      autoAlpha:0.5,
+    });
+    let tl = gsap.timeline();
+    tl.to(".box",{
+      duration:1,
+      width:"19vw",
+      delay:0.5,
+      ease:"power4",
+      opacity:1,
+    }).from(".hi",{
+      duration:1,
+      y:"7vw",
+      ease:"power3",
+      onComplete:()=> mastertl.play(),
+    }).to(".box",{
+      duration:1,
+      height:"7vw",
+      ease:"elastic",
+    });
+    let mastertl = gsap.timeline({repeat:-1}).pause();
+    words.forEach(word=>{
+      let tl = gsap.timeline({repeat:1, yoyo:true, repeatDelay:1})
+      tl.to(".text",{duration:1, text: word})
+      mastertl.add(tl)
+    })
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="App">
+      <span className='box'></span>
+      <span className='hi'>Hi,I am</span>
+      <span className='text'></span>
+      <span className='cursor'>_</span>
+    </h1>
   );
 }
 
